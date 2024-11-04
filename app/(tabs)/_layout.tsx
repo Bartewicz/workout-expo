1aee3de9df98a886b296dd3b0e46cd4f641ae232
Bartosz Wojtalewicz <bartoszwojtalewicz@pro.wp.pl>
2024-11-05 00:00:32 +0100

    style: improve tab bar styles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,39 +1,48 @@
-import { Tabs } from "expo-router";
-import React from "react";
+import { Tabs } from 'expo-router';
+import React from 'react';
 
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-import { Colors } from "@/constants/Colors";
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors.common.tint,
         headerShown: false,
+        tabBarActiveTintColor: Colors.common.primaryLighter,
+        tabBarInactiveTintColor: Colors.text.ultraLight,
+        tabBarActiveBackgroundColor: Colors.background.primary,
+        tabBarItemStyle: {
+          padding: 8,
+          gap: 5,
+        },
+        tabBarStyle: {
+          backgroundColor: Colors.background.primaryDark,
+          borderTopColor: Colors.common.tint,
+          height: 60,
+        },
       }}
       initialRouteName="(workout)/index"
     >
       <Tabs.Screen
         name="stats"
         options={{
-          title: "Stats",
+          title: 'Stats',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "stats-chart" : "stats-chart-outline"}
-              color={color}
-            />
+            <TabBarIcon name={focused ? 'stats-chart' : 'stats-chart-outline'} color={color} />
           ),
         }}
       />
       <Tabs.Screen
         name="(workout)"
         options={{
-          title: "Workout",
+          title: 'Workout',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
-              name={focused ? "barbell-sharp" : "barbell-outline"}
+              name={focused ? 'barbell-sharp' : 'barbell-outline'}
               color={color}
-              style={{ transform: [{ rotate: "-45deg" }] }}
+              size={32}
+              style={{ transform: [{ rotate: '-45deg' }] }}
             />
           ),
         }}
@@ -41,12 +50,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name="history"
         options={{
-          title: "History",
+          title: 'History',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "calendar" : "calendar-outline"}
-              color={color}
-            />
+            <TabBarIcon name={focused ? 'calendar' : 'calendar-outline'} color={color} />
           ),
         }}
       />
